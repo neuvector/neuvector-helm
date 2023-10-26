07215d5613121d2f8027d48b9c056b6b1f7e1848
Sam Wang (holyspectral) <sam.wang@suse.com>
2023-10-26 13:17:07 -0400

    feat: internal cert rotation
    feat: dynamic read internal certs from secret
    
    feat: override internal certs using emptyDir
    
    This is to support OpenShift where internal cert folder is not
    writeable.
    
    feat: upgrader only triggered in certain conds
    
    1. controller image tag is changed, e.g., upgrade.
    2. internal cert setting is changed, e.g., internal.certmanager
    3. upgrader setting is changed, e.g., controller.upgrader.env.
    
    feat: prevent old version broken by new charts
    
    This is to support OpenShift operator where new charts can be used for
    older version of NeuVector.
    
    feat: support running upgrader as a cronjob
    
    feat: allow image pull policy to be configured
    
    fix: support running in a different namespace
    
    fix: cronjob concurrency and history settings
    
    fix: make sure cronjob will be disabled
    
    feat: set job level timeout as 3600 seconds
    
    feat: add job timeout
    
    fix: version check for pre 5.3.1
    
    fix: make sure lease objects are deleted
    
    feat: support least privileges
    
    fix: rename upgrader to cert-upgrader

--- test/role_test.go
+++ test/role_test.go
@@ -68,7 +68,7 @@ func TestRoleLeastPrivilege(t *testing.T) {
 	out := helm.RenderTemplate(t, options, helmChartPath, nvRel, []string{"templates/role-least.yaml"})
 	outs := splitYaml(out)
 
-	if len(outs) != 1 {
+	if len(outs) != 3 {
 		t.Errorf("Resource count is wrong. count=%v\n", len(outs))
 	}
 
@@ -98,7 +98,7 @@ func TestRoleBindingLeastPrivilege(t *testing.T) {
 	out := helm.RenderTemplate(t, options, helmChartPath, nvRel, []string{"templates/rolebinding-least.yaml"})
 	outs := splitYaml(out)
 
-	if len(outs) != 2 {
+	if len(outs) != 5 {
 		t.Errorf("Resource count is wrong. count=%v\n", len(outs))
 	}
 }
