b3e1581921a7d5643483bc55599a936f0b7c06f1
wlin <wlin@neuvector.com>
2024-07-25 03:40:06 -0700

    NVSHAS-9102: New CRDs(2nd design) are required for Rancher RBAC integration with NeuVector in helm chart

--- test/crd_test.go
+++ test/crd_test.go
@@ -10,14 +10,16 @@ func TestCRD(t *testing.T) {
 	helmChartPath := "../charts/crd"
 
 	options := &helm.Options{
-		SetValues: map[string]string{},
+		SetValues: map[string]string{
+			"controller.ranchersso.enabled": "true",
+		},
 	}
 
 	// Test ingress
 	out := helm.RenderTemplate(t, options, helmChartPath, nvRel, []string{"templates/crd.yaml"})
 	outs := splitYaml(out)
 
-	if len(outs) != 8 {
+	if len(outs) != 24 {
 		t.Errorf("Resource count is wrong. count=%v\n", len(outs))
 	}
 }
@@ -26,14 +28,16 @@ func TestCoreCRD(t *testing.T) {
 	helmChartPath := "../charts/core"
 
 	options := &helm.Options{
-		SetValues: map[string]string{},
+		SetValues: map[string]string{
+			"controller.ranchersso.enabled": "true",
+		},
 	}
 
 	// Test ingress
 	out := helm.RenderTemplate(t, options, helmChartPath, nvRel, []string{"templates/crd.yaml"})
 	outs := splitYaml(out)
 
-	if len(outs) != 8 {
+	if len(outs) != 24 {
 		t.Errorf("Resource count is wrong. count=%v\n", len(outs))
 	}
 }
@@ -44,6 +48,7 @@ func TestCoreCRDDisabled(t *testing.T) {
 	options := &helm.Options{
 		SetValues: map[string]string{
 			"crdwebhook.enabled": "false",
+			"controller.ranchersso.enabled": "false",
 		},
 	}
 
@@ -52,6 +57,25 @@ func TestCoreCRDDisabled(t *testing.T) {
 	outs := splitYaml(out)
 
 	if len(outs) != 0 {
+		t.Errorf("Resource count is wrong. count=%v\n", len(outs))
+	}
+}
+
+func TestCoreCRDDisabledRancherSSOEnabled(t *testing.T) {
+	helmChartPath := "../charts/core"
+
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"crdwebhook.enabled": "false",
+			"controller.ranchersso.enabled": "true",
+		},
+	}
+
+	// Test ingress
+	out, _ := helm.RenderTemplateE(t, options, helmChartPath, nvRel, []string{"templates/crd.yaml"})
+	outs := splitYaml(out)
+
+	if len(outs) != 16 {
 		t.Errorf("Resource count is wrong. count=%v\n", len(outs))
 	}
 }
